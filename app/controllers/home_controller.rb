de96f69884289e348c6b3c944c7c2460d02b4a4e
Jillian Tang <jilingt@stanford.edu>
2019-02-16 10:20:07 -0800

    Added bootsnipp template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+
+  end
+  def test
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'home/index'
-  root 'home#index'
+  get 'home/test'
+  root 'home#test'
 end
