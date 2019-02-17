ef8a190837e6e00b856565ec0ebfe36e3888a8bc
Ethan Chi <ethanchi@stanford.edu>
2019-02-16 20:55:37 -0800

    fixing errors

--- main.py
+++ main.py
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 # [START gae_flex_quickstart]
-import logging, numpy
+import logging
+import numpy as np
+
+from math import ceil
 
 from flask import Flask, render_template, request, jsonify
 from google.cloud import datastore
@@ -51,7 +54,7 @@ def printSymptomsWeights(probabilityScores):
         return str(out)
     
 def generateQuestionForPatient(patient):
-    import numpy as np
+    print(patient['weights'])
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
     symptomsFile = open('symptomsList.txt', 'r')
@@ -72,50 +75,94 @@ def generateQuestionForPatient(patient):
     
     runningScores = np.c_[diseasesNP, np.zeros((len(diseases), 1))]
     
-    weights = np.ndarray(patient['weights'])    
+    weights = np.array(patient['weights'])    
     logging.info(weights)
     logging.info(calculateProbabilityScore(mappingsArr, weights))
     runningScores[:, 1] = np.squeeze(calculateProbabilityScore(mappingsArr, weights))
     runningScoresNew = runningScores[runningScores[:,1].astype(np.float).argsort()[::-1]]
     logging.info(runningScoresNew)
-    subset = np.squeeze(runningScoresNew[0:5, 0])
+    numTop = 5 # ceil(len(diseases) - (len(diseases)/len(symptoms)) * (numQuestions - 1))
+    subset = np.squeeze(runningScoresNew[0:numTop, 0])
     indices = np.vectorize(lambda x: diseases.index(x))(subset)
     mappingsArrNew = mappingsArr[indices]
     stdevs = np.std(mappingsArrNew, axis=0)
     ranking = list(np.argsort(stdevs)[::-1])
-    doneIndices = list(np.nonzero(runningWeights)[0])
+    doneIndices = list(np.nonzero(weights)[0])
     remaining = list(filter(lambda x : x not in doneIndices, ranking))
-    best = remaining[0]
-    logging.info("Best index is: {}.")
-    return best
+    bestSymptom = symptoms[remaining[0]]
+    options = {"Yes": "1", "Maybe": "0", "No": "-0.5"}
+    
+    questions = open('questions.txt', 'r')
+    questions = dict([map(lambda x: x.lower().strip(), line.split(': ')) for line in questions.readlines()])
+    question = questions[bestSymptom]
+    
+    return question, options, bestSymptom
 
 
 @app.route('/createPatientID', methods=['POST'])
 def createPatientID():
     ds = datastore.Client()
-
-
-    logging.info('hi')
-    logging.info('hi')
+    logging.info(request.mimetype)
+    logging.info(request.data)
     info = request.get_json()
+    logging.info("Test2!!")
     age = info['age']
     sex = info['sex']
     weights = [0] * NUM_SYMPTOMS
     entity = datastore.Entity(key=ds.key('patient'))
-    entity.update({
-        'age': age,
-        'sex': sex,
-        'weights': weights,
-    })
+    entity['age'] = age
+    entity['sex'] = sex
+    entity['weights'] = weights
 
     ds.put(entity)
 
     patientKey = str(entity.key.to_legacy_urlsafe())
-    
+    print(entity['weights'])
+
+    question, options, questionID = generateQuestionForPatient(entity)
     
     output = {
         "ID": patientKey,
-        "question": generateQuestionForPatient(entity)
+        "question": question,
+        "options": options,
+        "questionID": questionID
+    }
+    return jsonify(output)
+
+@app.route('/receiveResponse', methods=['POST'])
+def receiveResponse():
+    ds = datastore.Client()
+    info = request.get_json()
+    patientID = info['patientID']
+    patientID = patientID[2:-1]
+    questionID = info['questionID']
+    answer = info['answer']
+    
+    weights = [0] * NUM_SYMPTOMS
+    entity = ds.get(datastore.key.Key.from_legacy_urlsafe(patientID))
+
+    
+
+    
+    
+    # we find the index we need to update
+    
+    symptomsFile = open('symptomsList.txt', 'r')
+    symptoms = [x.strip() for x in symptomsFile.readlines()]
+    symptomsFile.close()
+    
+    index = symptoms.index(questionID)
+    entity['weights'][index] = float(answer)
+    
+    ds.put(entity)
+    
+    question, options, questionID = generateQuestionForPatient(entity)
+    
+    output = {
+        "ID": patientID,
+        "question": question,
+        "options": options,
+        "questionID": questionID
     }
     return jsonify(output)
 
